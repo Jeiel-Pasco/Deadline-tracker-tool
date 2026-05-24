d24a600d7b377410b8a0b7a5f170d8f399cc28cf
Jeiel Pasco <jeielpasco13@gmail.com>
2026-05-24 14:16:48 +0800

    Update password input field styling

--- pages/login.js
+++ pages/login.js
@@ -77,7 +77,7 @@ export default function Login() {
                   required
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
-                  className="appearance-none block w-full px-3 py-2 pr-10 border border-slate-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-slate-900"
+                  className="appearance-none block w-full px-3 py-2 pr-12 border border-slate-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-slate-900 bg-white"
                   placeholder="••••••••"
                 />
                 <button
